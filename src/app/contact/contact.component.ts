1cce95545a95bccd3ab93c48f5d6a9044acccedb
SmallPeenwow <bowdencamryn@gmail.com>
2024-02-08 12:14:18 +0200

    6.4 Writing Custom Validators Done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { createInvalidDomainValidator } from './invalidEmailDomain';
+
+const invalidEmailDomain = createInvalidDomainValidator([
+  'gmail.com',
+  'yahoo.com',
+  'hotmail.com',
+]);
 
 @Component({
   selector: 'app-contact',
@@ -13,7 +20,11 @@ export class ContactComponent implements OnInit {
 
   contactForm = new FormGroup({
     senderName: new FormControl('', Validators.required),
-    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderEmail: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      invalidEmailDomain,
+    ]),
     senderMessage: new FormControl('', [
       Validators.required,
       Validators.minLength(10),
