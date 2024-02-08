47c153043ed096f82df54ac8c23dae3dddac9636
SmallPeenwow <bowdencamryn@gmail.com>
2024-02-08 11:12:56 +0200

    6.3 Validating Reactive Forms Done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -12,9 +12,12 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {}
 
   contactForm = new FormGroup({
-    senderName: new FormControl(''),
-    senderEmail: new FormControl(''),
-    senderMessage: new FormControl(''),
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
   });
 
   submitForm() {
